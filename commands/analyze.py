5bf592e1ab65277bef7043a692a33e32b2d3f46f
majid-gt <mdmajid743@gmail.com>
2026-03-02 01:57:12 +0530

    ESP32 hardware vault integration completely done, unlocking via serial esp and changing password

--- commands/analyze.py
+++ commands/analyze.py
@@ -1,5 +1,5 @@
 from main import run_analysis_pipeline
 
-def run_analyze():
+def run_analyze(hardware_auth=False):
     print("Running cloud analysis...\n")
-    run_analysis_pipeline()
+    run_analysis_pipeline(hardware_auth=hardware_auth)
